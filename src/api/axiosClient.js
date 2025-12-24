3457edd9e1236ee20fb77e28fa52bc57646db8d2
DoanTTung1 <photonics.pixel02558@gmail.com>
2025-12-25 02:02:48 +0700

    Doi URL

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    // 👇 SỬA DÒNG NÀY:
-    // 1. Đổi http -> https (Railway bắt buộc bảo mật)
-    // 2. Thay localhost:8080 -> thanhtungf.up.railway.app
-    baseURL: 'https://thanhtungf.up.railway.app/api/admin/', 
-    
+
+    baseURL: 'https://thanhtung-building.up.railway.app/api/admin',
+
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
-export default axiosClient;
+export default axiosClient;  
