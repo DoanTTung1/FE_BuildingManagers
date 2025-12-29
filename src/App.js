faf87b82ea62fbb660ba6dd57ea198c495f79705
DoanTTung1 <photonics.pixel02558@gmail.com>
2025-12-29 15:54:55 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
+import React from 'react';
+// Lưu ý: Đổi tên từ 'Router' thành 'BrowserRouter' để chuẩn hóa
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import AuthModal from './components/AuthModal';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './components/HomePage';
+import BuildingSearch from './components/BuildingSearch';
+import BuildingDetail from './components/BuildingDetail';
+import CreateBuilding from './components/CreateBuilding';
+import AdminLayout from './layouts/AdminLayout';
+import BuildingManager from './pages/admin/BuildingManager';
+import UserManager from './pages/admin/UserManager';
+
+import './styles/App.css';
+
 function App() {
-  return (
-    // ĐƯA ROUTER RA NGOÀI CÙNG
-    <Router>
-      <AuthProvider> {/* Bọc AuthProvider bên trong Router */}
-        <div className="App" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-          <Header />
+    return (
+        // 1. BrowserRouter phải là bọc ngoài cùng nhất
+        <BrowserRouter>
+            {/* 2. AuthProvider nằm trong Router để sử dụng được useNavigate() */}
+            <AuthProvider>
+                <div className="App" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+                    <Header />
 
-          <main style={{ paddingTop: '100px', flex: 1, backgroundColor: '#f8fafd' }}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/search" element={<BuildingSearch />} />
-              <Route path="/building/:id" element={<BuildingDetail />} />
-              <Route path="/post-building" element={<CreateBuilding />} />
+                    <main style={{ paddingTop: '100px', flex: 1, backgroundColor: '#f8fafd' }}>
+                        <Routes>
+                            {/* Public Routes */}
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/search" element={<BuildingSearch />} />
+                            <Route path="/building/:id" element={<BuildingDetail />} />
+                            <Route path="/post-building" element={<CreateBuilding />} />
 
-              {/* --- ROUTE CHO ADMIN --- */}
-              <Route path="/admin" element={<AdminLayout />}>
-                <Route index element={<BuildingManager />} />
-                <Route path="buildings" element={<BuildingManager />} />
-                <Route path="users" element={<UserManager />} />
-              </Route>
-            </Routes>
-          </main>
+                            {/* Admin Routes */}
+                            <Route path="/admin" element={<AdminLayout />}>
+                                <Route index element={<BuildingManager />} />
+                                <Route path="buildings" element={<BuildingManager />} />
+                                <Route path="users" element={<UserManager />} />
+                            </Route>
+                        </Routes>
+                    </main>
 
-          <Footer />
+                    <Footer />
 
-          {/* Modal nằm ở đây đã có thể sử dụng navigate() từ AuthContext */}
-          <AuthModal />
-        </div>
-      </AuthProvider>
-    </Router>
-  );
+                    {/* Modal hiển thị toàn cục */}
+                    <AuthModal />
+                </div>
+            </AuthProvider>
+        </BrowserRouter>
+    );
 }
+export default App;
